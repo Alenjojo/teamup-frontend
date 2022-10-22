198636add03dcd3b2cadc79723456a2a5adfbf05
Alen Jojo <alen.j@menerva.tech>
2022-10-22 11:46:21 +0530

    added folder url

--- src/Component/Contents.js
+++ src/Component/Contents.js
@@ -11,9 +11,21 @@ export default function Content() {
       </div>
 
       <div className="folders">
-        <img className="green" src="./images/green_folder.png"></img>
-        <img className="black" src="./images/black_folder.png"></img>
-        <img className="blue" src="./images/blue_folder.png"></img>
+        <img
+          className="green"
+          alt="Green Folder"
+          src="https://firebasestorage.googleapis.com/v0/b/gggg-55aad.appspot.com/o/website%2Fgreen_folder.webp?alt=media&token=e7fdf43b-2c5a-4a6f-9214-a23cd823fc00"
+        ></img>
+        <img
+          className="black"
+          alt="Black Folder"
+          src="https://firebasestorage.googleapis.com/v0/b/gggg-55aad.appspot.com/o/website%2Fblack_folder.webp?alt=media&token=2ac22d71-b25a-41cf-b0fa-b144b4b1c2b8"
+        ></img>
+        <img
+          className="blue"
+          alt="Blue Folder"
+          src="https://firebasestorage.googleapis.com/v0/b/gggg-55aad.appspot.com/o/website%2Fblue_folder.webp?alt=media&token=4b3940d8-7154-4ad0-9a4b-eb8c7451be64"
+        ></img>
       </div>
     </div>
   );
