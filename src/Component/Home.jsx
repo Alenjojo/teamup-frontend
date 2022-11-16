2322af9970bee2c45a00d40738770ca0948c0260
Alen Jojo <alenjojo252001@gmail.com>
2022-11-16 15:56:43 +0530

    Added about

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import About from "./About";
 
 class Home extends Component {
   state = {};
   render() {
-    return <div>Home</div>;
+    return (
+      <div>
+        <div className="flex justify-between h-screen">
+          <div className="bg-stone-700 w-[20%]"></div>
+          <div className="bg-slate-400 w-[55%]">
+            <About />
+          </div>
+          <div className="bg-orange-500 w-[25%]"></div>
+        </div>
+      </div>
+    );
   }
 }
 
